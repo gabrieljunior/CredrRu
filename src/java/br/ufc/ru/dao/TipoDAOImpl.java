0a1d24010e54b19a3fb3c669ea74fbaac28d6809
valpinheiro <38711118+valpinheiro@users.noreply.github.com>
2018-06-09 22:20:25 -0300

    Versao 10.0

--- src/java/br/ufc/ru/dao/TipoDAOImpl.java
+++ src/java/br/ufc/ru/dao/TipoDAOImpl.java
@@ -1,43 +1,71 @@
 package br.ufc.ru.dao;
 
 import br.ufc.ru.model.Tipo;
+import br.ufc.util.HibernateUtil;
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-public class TipoDAOImpl {
-    static ArrayList<Tipo> tipos = new ArrayList<Tipo>();
-    
-    /*public TipoDAOImpl(){
-        tipos = new ArrayList<Tipo>();
-    }
-    
-    @Override
-    public List<Tipo> getTodosTipos() {
-        return tipos;
-    }
+public class TipoDAOImpl implements TipoDao {
 
     @Override
-    public Tipo getTipo(int codigo) {
-        for(int i = 0; i < tipos.size(); i++){
-            if(tipos.get(i).getCodigo() == codigo){
-                return tipos.get(i);
-            }
+    public Tipo getTipo(int id) {
+        Tipo tipo = null;
+        Transaction trns = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            trns = session.beginTransaction();
+            //String queryString = "from User where id = :id";
+           // Query query = session.createQuery(queryString);
+           // usuario = (Usuario) query.uniqueResult();
+           tipo = (Tipo) session.get(Tipo.class, id);
+                    
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+           // session.flush();
+            session.close();
         }
-        return null;
+        return tipo;
+
+    }
+    
+    @Override
+    public Tipo getTipo(String nomeTipo){
+        Tipo tipo = null;
+        Transaction trns = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            trns = session.beginTransaction();
+           String queryString = "from tipo where nometipo = :nomeTipo";
+           Query query = session.createQuery(queryString);
+           tipo = (Tipo) query.uniqueResult();
+           //tipo = (Tipo) session.get(Tipo.class, id);
+                    
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+           // session.flush();
+            session.close();
+        }
+        return tipo;
     }
 
     @Override
     public void updateTipo(Tipo tipo) {
-        adicionarTipo(getTipo(tipo.getCodigo()));
+      
     }
 
     @Override
-    public void deleteTipo(Tipo tipo) {
-         tipos.remove(getTipo(tipo.getCodigo()));
+    public void deleteTipo(int id) {
+        
+    }
+
+    @Override
+    public void adicionarTipo(Tipo tipo) {
+        
     }
     
-    @Override
-    public void adicionarTipo(Tipo tipo){
-        tipos.add(tipo);
-    */
 }
